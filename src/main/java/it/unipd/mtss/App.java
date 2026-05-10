37c2373aa04e0d9a18ab9f09265ea94bf21a24c0
ken <chendaxin04@gmail.com>
2026-05-10 17:09:04 +0200

    implementato entry point App per generazione JAR eseguibile

--- src/main/java/it/unipd/mtss/App.java
+++ src/main/java/it/unipd/mtss/App.java
@@ -1,13 +1,21 @@
 package it.unipd.mtss;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+
+public class App {
+    public static void main(String[] args) {
+        int numberToPrint = 50;
+        
+        try {
+            System.out.println("Numero arabo: " + numberToPrint);
+            
+            String asciiArt = RomanPrinter.print(numberToPrint);
+            
+            System.out.println("In formato Romano ASCII Art:");
+            System.out.println(asciiArt);
+            
+        } catch (IllegalArgumentException e) {
+            System.err.println("Errore: " + e.getMessage());
+        }
+        
     }
 }
